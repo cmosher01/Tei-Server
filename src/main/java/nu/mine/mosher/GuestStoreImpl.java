5e2163f4ba7448d2b6c88c86a3fd6ae1a4b2f265
Christopher Mosher <cmosher@coreinformatics.com>
2018-09-17 15:48:43 -0400

    read guest pass from env var

--- src/main/java/nu/mine/mosher/GuestStoreImpl.java
+++ src/main/java/nu/mine/mosher/GuestStoreImpl.java
@@ -1,15 +1,24 @@
 package nu.mine.mosher;
 
+import nu.mine.mosher.security.password.StrongHash;
+
 /**
  * Example Credentials.Store that has only only user, "guest",
  * with a strong password (that we only store the uncrackable hash of).
  */
 final class GuestStoreImpl implements Credentials.Store {
     private static final String GUEST_USERNAME = "guest";
-    private static final String GUEST_PASSWORD_HASH =
-        "101021:" +
-        "1068F32040220866435C989DA3A120F7:" +
-        "17DB77FDEC8E7C5E348E5C435C669F206AB47A4402337FB353266DBA59375AE85ACB9C83C37E3DB0C08DF59F2449639002E27B57716EA3DB069B345B7E247968";
+    private static final String GUEST_PASSWORD_HASH;
+    static {
+        String h = "";
+        try {
+            h = StrongHash.hash(System.getenv("GUEST_PASSWORD"));
+        } catch (final Throwable e) {
+            h= "";
+        }
+        GUEST_PASSWORD_HASH = h;
+    }
+
     private static final Credentials.Store INSTANCE = new GuestStoreImpl();
 
     private GuestStoreImpl() {
